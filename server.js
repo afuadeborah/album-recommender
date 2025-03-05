62e7f139f528cdc15097ec611b2eb9eafcf40a0a
Afua <afrimpong.15@gmail.com>
2025-03-05 17:31:24 -0500

    wip: refactor db file, install cors, set up MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 import dotenv from "dotenv";
 import express from 'express';
-import './database.js';
+import cors from "cors";
 import authRouter from "./backend/routes/auth.js";
 import noteRouter from "./backend/routes/note.js";
+import connectDatabase from "./database.js";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+// allows cross-origin requests (Postman)
+app.use(cors());
+
+connectDatabase();
+
 app.use('/api/auth', authRouter);
 app.use('/api/notes', noteRouter);
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
